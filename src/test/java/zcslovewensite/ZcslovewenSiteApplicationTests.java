1fc658a0d98fedb951749bfd7ed452799da5078f
ZCS2016 <basketbasket0@126.com>
2018-04-25 18:23:37 +0800

    Create website project

---
+++ src/test/java/zcslovewensite/ZcslovewenSiteApplicationTests.java
@@ -0,0 +1,16 @@
+package zcslovewensite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZcslovewenSiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
